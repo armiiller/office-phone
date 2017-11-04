8e95c4bdb32067ab29ecd4709c7bc59adf8730b6
Austin <austinrmiller1991@gmail.com>
2017-11-03 17:08:21 -0700

    Minor changes to our example project jsons. Adds the code to our record and record status functions

--- functions/record/index.js
+++ functions/record/index.js
@@ -1,5 +1,46 @@
-console.log('starting function')
+console.log('starting function');
+
+const qs = require('qs');
+const VoiceReponse = require('twilio').twiml.VoiceResponse;
+
 exports.handle = function(e, ctx, cb) {
   console.log('processing event: %j', e)
-  cb(null, {})
+  var twiml = new VoiceReponse();
+
+  var noanswerurl             = process.env.NOANSWERURL || null;
+  var thanksformessageurl     = process.env.THANKSFORMESSAGEURL || null;
+  var recordingstatuscallback = process.env.RECORDINGSTATUSURL || '/recordstatus';
+  var messagemaxlength        = process.env.MESSAGEMAXLENGTH ? parseInt(process.env.MESSAGEMAXLENGTH) : 30;
+
+  var body = {};
+  if(e && e.reqbody){
+    body = qs.parse(e.reqbody);
+  }
+
+  switch(body.DialCallStatus){
+    case 'no-answer':
+      if(noanswerurl){
+        twiml.play(noanswerurl);
+      }else {
+        twiml.say("Sorry we couldn't get to the phone, please leave a message after the beep.");
+      }
+      twiml.record({ maxLength: messagemaxlength, recordingStatusCallback: recordingstatuscallback});
+      break;
+    default:
+      if(thanksformessageurl){
+        twiml.play(thanksformessageurl);
+      } else {
+        twiml.say("Thanks for your message! We will get back to you soon. Goodbye.");
+      }
+      twiml.hangup();
+      break;
+  }
+
+  var responseBody = twiml.toString();
+  var response = {
+      statusCode: 200,
+      body: responseBody
+  };
+
+  cb(null, response);
 }
--- functions/recordstatus/index.js
+++ functions/recordstatus/index.js
@@ -1,5 +1,24 @@
-console.log('starting function')
+console.log('starting function');
+
+const qs = require('qs');
+const snsPublish = require('aws-sns-publish');
+
 exports.handle = function(e, ctx, cb) {
   console.log('processing event: %j', e)
-  cb(null, {})
+  var snsarn = process.env.SNSARN;
+
+  var body = {};
+  if(e && e.reqbody){
+    body = qs.parse(e.reqbody);
+  }
+
+  if(snsarn){
+    snsPublish(`New Voicemail: ${body.RecordingUrl}`, {arn: snsarn, subject: 'New Voicemail'});
+  }
+
+  var response = {
+      statusCode: 200,
+  };
+
+  cb(null, response);
 }
